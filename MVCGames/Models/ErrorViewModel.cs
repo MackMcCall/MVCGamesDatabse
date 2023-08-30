a068ac7f2a7bb74bf273e6c12038687f354c3a7c
Mack McCall <mackmc9924@gmail.com>
2023-08-30 13:28:29 -0600

    configured git ignore

---
+++ MVCGames/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCGames.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
